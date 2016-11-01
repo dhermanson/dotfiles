2abf7f99b74150d715d8e0383f29af0a6cd30ded
Derick Hermanson <dhermanson@myriadmobile.com>
2016-11-01 11:08:33 -0500

    Add php ctags scripts for emacs

---
+++ bash/create-php-ctags-emacs.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+ctags -R --fields=K --PHP-kinds=zty --languages=php --exclude=test --exclude=tests --exclude=spec --exclude=specs --exclude=Test --exclude=Tests --exclude=Spec --exclude=Specs -e
+
